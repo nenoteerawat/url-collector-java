002d18b06aa70f46c60b28dd131a950ff9082631
nattachai290 <nattachai6317@gmail.com>
2018-11-09 13:34:17 +0700

    change react to angular 7

--- frontend/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import MainLayout from './container/layout/MainLayout.jsx';
-
-ReactDOM.render(<MainLayout/>, document.getElementById('root'));
-
-
-
